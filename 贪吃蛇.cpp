fe2a71e753e7a8784579119e85149841f345dbfe
zy-98 <59222884+zy-98@users.noreply.github.com>
2020-01-08 19:35:42 +0800

    Create 贪吃蛇.cpp

---
+++ 贪吃蛇.cpp
@@ -0,0 +1,16 @@
+// 贪吃蛇.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "unit.h"
+#include "food.h"
+#include "snack.h"
+#include <conio.h>
+#include "control.h"
+using namespace std;
+int main()
+{	
+	control game;
+	game.run();
+}
